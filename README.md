f9ee4e976341b6183cb36f637e95bde017a2860b
NorwegianLamb <norwegianlamb@protonmail.com>
2023-03-19 17:19:18 +0100

    .

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # HealthBuddy
 Testings startup web&amp;app implementations
+"LH/server -> 'npm run dev' "
+"LH/client -> 'npm start' "
+
+server running on 5001, client on 3000 with a LH:5001 proxy to fetch /api
--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 
 app.get("/api", (req, res) =>{
-    res.json({"users": ["userOne", "userTwo", "userThree", "userFour"]})
+    res.json({"doct1": ["userOne", "userTwo", "userThree", "userFour"],
+                "doct2": ["", "", ""],
+            })
 })
 
 app.listen(5001, () => {console.log("Server started on port 5001")})
