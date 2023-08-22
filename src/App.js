34bbd44971639820ec8e5065adcc6b0c0cfb518d
NorwegianLamb <norwegianlamb@protonmail.com>
2023-08-22 18:16:56 +0200

    main section, fixes on navbar
    basic overlay for the moment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const images = ['image1.png', 'image2.png', 'image3.png', 'image4.png'];
+
+  const handleSquareClick = (index) => {
+    setCurrentIndex(index);
+  };
+
+  // Automatic image rotation every 5 seconds
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((currentIndex + 1) % images.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [currentIndex]);
+
   return (
     <div className="App">
       <header className="homepage_header">
@@ -17,16 +34,40 @@ function App() {
               <li><a href="/centri-sanitari">Centri Sanitari</a></li>
             </ul>
           </nav>
-          </div>
-        <div>
-          <a className="nav-icon" href="/notifiche"><button>n</button></a>
-          <a className="nav-icon" href="/impostazioni"><button>i</button></a>
-          <a className="nav-icon" href="/area-personale"><button>Area Personale</button></a>
+        </div>
+        <div className='right_nav'>
+          <a href="/notifiche" class="image-button">
+            <img src={process.env.PUBLIC_URL + '/notification-bell.png'} alt="notifiche" />
+          </a>
+          <a href="/impostazioni" class="image-button">
+            <img src={process.env.PUBLIC_URL + '/settings.png'} alt="impostazioni" />
+          </a>
+          <a href="/area-personale" class="image-button">
+            <img src={process.env.PUBLIC_URL + '/profile.png'} alt="area-personale" />
+          </a>
         </div>
       </header>
+      
       <main>
-        <h1>Test</h1>
+        <div className="left-content">
+          Scegli il <span>professionista</span> più <span>adatto a te</span>
+        </div>
+        <div className="right-content">
+          <div className="image-container">
+            <img src={process.env.PUBLIC_URL + '/images/main/' + images[currentIndex]} alt={`Image ${currentIndex + 1}`} id="main-image" />
+          </div>
+          <div className="image-selector">
+            {images.map((image, index) => (
+              <div
+                key={index}
+                className={`square ${index === currentIndex ? 'active' : ''}`}
+                onClick={() => handleSquareClick(index)}
+              />
+            ))}
+          </div>
+        </div>
       </main>
+
       <footer>
         <p>Test2</p>
       </footer>
