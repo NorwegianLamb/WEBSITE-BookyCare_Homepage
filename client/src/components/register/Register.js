f121c8836055dda1adad94e7b7c1b60c0ca0b4aa
NorwegianLamb <norwegianlamb@protonmail.com>
2023-10-25 14:54:38 +0200

    studying routers

---
+++ client/src/components/register/Register.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+function Register() {
+    let history = useHistory()
+
+    return (
+        <>
+            <div>
+                <h1>Registration Test</h1>
+                <button onClick={() => history.push("/profile")}>Register</button>
+            </div>
+        </>
+    );
+}
+
+export default Register;
