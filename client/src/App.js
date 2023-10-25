97cf6868a7fa47517807b80073504fd558d34081
NorwegianLamb <norwegianlamb@protonmail.com>
2023-10-25 16:12:22 +0200

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'; // Import React Router components
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import './App.css';
 import Loading from './components/loading/Loading';
 import Homepage from './components/homepage/Homepage';
@@ -15,12 +15,14 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      {
-        loading ? (<Loading />) 
-        : (<Homepage />)
-      }
-    </div>
+    <Router>
+      <div className="App">
+          {
+            loading ? (<Loading />) 
+            : (<Homepage />)
+          }
+      </div>
+    </Router>
   );
 }
 
